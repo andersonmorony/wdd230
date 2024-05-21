88bc9c8d226a824bf8fb28f249617912bc3d3250
Anderson Moroni <andersonmoroni92@gmail.com>
2024-05-21 07:36:26 -0300

    changes

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,14 +1,17 @@
 const hamButton = document.querySelector('#btn-menu');
 const navigation = document.querySelector('.navigation');
-const btnJoin = document.getElementById("btn-join");
+const btnJoin = document.getElementsByClassName("btn-join");
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
 
+var buttonsArray = Array.from(btnJoin);
 
-btnJoin.addEventListener('click', () => {
-	window.location.href = "join.html"
-})
+buttonsArray.forEach(element => {
+	element.addEventListener('click', () => {
+		window.location.href = "join.html"
+	})
+});
 
